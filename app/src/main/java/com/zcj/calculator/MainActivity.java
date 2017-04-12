ee5f9887face3fa3106178ece9f1ed99d2d3da63
zcj <canjiezeng@gmail.com>
2017-04-12 14:31:33 +0800

    做了一些修改

--- app/src/main/java/com/zcj/calculator/MainActivity.java
+++ app/src/main/java/com/zcj/calculator/MainActivity.java
@@ -1,16 +1,25 @@
 package com.zcj.calculator;
 
 import android.app.Activity;
+import android.app.Dialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.InputType;
 import android.text.TextWatcher;
+import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.ads.AdView;
+import com.guowei.guowei_general.ADSystem.MoreActivity;
+import com.guowei.guowei_general.ADSystem.XMLUtils;
+
+import java.io.Serializable;
 import java.lang.reflect.Method;
 import java.math.BigDecimal;
 
@@ -24,12 +33,15 @@ public class MainActivity extends Activity {
     private TextView tv_calculat;
     private int index=0;
     private Editable editable;
+    private AdView adView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         et_show = (EditText) findViewById(R.id.et_show);
+        adView = (AdView) findViewById(R.id.adView);
+        AdsDialogUtil.setBannerAds(adView);
         editable=et_show.getEditableText();
         et_show.addTextChangedListener(new TextWatcher() {
             @Override
@@ -57,9 +69,9 @@ public class MainActivity extends Activity {
             }
         });
         //5.0以上可以用"setShowSoftInputOnFocus"这条函数
-        if (android.os.Build.VERSION.SDK_INT <= 10) {//4.0以下
+        if (android.os.Build.VERSION.SDK_INT <= 10) {//3.0以下
             et_show.setInputType(InputType.TYPE_NULL);
-        } else {//4.0以上
+        } else {//3.0以上
             this.getWindow().setSoftInputMode(
                     WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
             try {
@@ -97,15 +109,16 @@ public class MainActivity extends Activity {
         }
         if (before_index.equals("+")||
                 before_index.equals("-")||
-                before_index.equals("*")||
-                before_index.equals("/")){
-            if (calculat.equals("-")){
-                if (before_index.equals("-")||before_index.equals("+")){
-                    editable.replace(index-1,index,calculat);
-                }else{
-                    editable.insert(index,calculat);
-                }
-            }else if(calculat.equals(".")) {
+                before_index.equals("×")||
+                before_index.equals("÷")){
+//            if (calculat.equals("-")){
+//                if (before_index.equals("-")||before_index.equals("+")){
+//                    editable.replace(index-1,index,calculat);
+//                }else{
+//                    editable.insert(index,calculat);
+//                }
+//            }else
+                if(calculat.equals(".")) {
             }
              else{
                 editable.replace(index - 1, index, calculat);
@@ -116,8 +129,8 @@ public class MainActivity extends Activity {
             int now_index=index-(text.length()-texts[texts.length-1].length());
             String B_index=index_text.substring(0,now_index);
             String A_index=index_text.substring(now_index,index_text.length());
-            String[] B_split = B_index.split("\\+|-|\\*|/");
-            String[] A_split = A_index.split("\\+|-|\\*|/");
+            String[] B_split = B_index.split("\\+|-|×|÷");
+            String[] A_split = A_index.split("\\+|-|×|÷");
             String B_number=B_split[B_split.length-1];
             String A_number=A_split[A_split.length-1];
             String index_number=B_number+A_number;
@@ -147,12 +160,9 @@ public class MainActivity extends Activity {
     public void BigC(View v){
         editable.delete(0, et_show.length());
     }
-    public void mmmm(View v){
-        Toast.makeText(MainActivity.this, "别碰我，我什么都不会。。", Toast.LENGTH_SHORT).show();
-    }
     public void Equal(View v){
         String first_text=et_show.getText().toString();
-        String text=first_text.replace("*-","aa");
+        String text=first_text.replace("×-","aa");
         text=text.replace("/-","bb");
         String result=count(text);
         if (isError){
@@ -170,7 +180,7 @@ public class MainActivity extends Activity {
             result=(int)jia(texts[texts.length-1])+"";
         }
         else{
-            result=jia(texts[texts.length-1])+"";
+            result=(float)jia(texts[texts.length-1])+"";
         }
         return result;
     }
@@ -213,7 +223,7 @@ public class MainActivity extends Activity {
         return sum;
     }
     private double chen(String text){
-        String[] chen=text.split("\\*");
+        String[] chen=text.split("×");
         double[] chens=new double[chen.length];
 
             for (int i = 0; i < chen.length; i++) {
@@ -259,20 +269,20 @@ public class MainActivity extends Activity {
         return sum;
     }
     private double chu(String text){
-        String[] chu=text.split("/");
+        String[] chu=text.split("÷");
         double[] chus=new double[chu.length];
         for (int i = 0; i < chu.length; i++) {
             if (chu[i].equals(".")){
                 chus[i]=0;
             }else if (chu[i].equals("")){
                 isError=true;
-                Error="内容不能为空";
+                Error=getResources().getString(R.string.content_not_be_blank);
             }else{
                 try {
                 chus[i] = new BigDecimal(chu[i]).doubleValue();
             }catch (Exception e){
                 isError=true;
-                Error="不能有文字";
+                Error=getResources().getString(R.string.can_not_have_characters);
                 return 0;
             }
             }
@@ -292,4 +302,37 @@ public class MainActivity extends Activity {
         return sum;
     }
 
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode==KeyEvent.KEYCODE_BACK){
+            exit();
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+    private void exit() {
+        AdsDialogUtil.showAdsDialog(this, new AdsDialogUtil.AdsDialogListener() {
+            @Override
+            public void onYesPress(Dialog dialog) {
+                dialog.dismiss();
+                finish();
+            }
+
+            @Override
+            public void onMorePress(Dialog dialog) {
+                MainActivity.this.startActivity(
+                        new Intent(MainActivity.this,MoreActivity.class)
+                                .putExtra(
+                                        "values",(Serializable) XMLUtils.getAppBeans(MainActivity.this)
+                                )
+                );
+            }
+
+            @Override
+            public void onNoPress(Dialog dialog) {
+                dialog.dismiss();
+            }
+        });
+    }
 }
